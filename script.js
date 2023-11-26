d1d934570e376986b66dc8f2e9d91abe2988e30d
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-11-25 20:48:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -73,21 +73,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
         submitButton.addEventListener('click', checkAnswer);
 
-        function logForTeam(team) {
-            console.log(`Point scored for the ${team} team.`);
-            feedbackElement.textContent = `Scored for ${team} team!`;
 
-            document.getElementById('team-options').style.display = 'none';
-            feedbackElement.textContent = '';
-            generateQuestion();
-        }
+ function logForTeam(team) {
+    console.log(`Point scored for the ${team} team.`);
+    feedbackElement.textContent = `Scored for ${team} team!`;
 
-        document.querySelectorAll('.team-button').forEach(button => {
-            button.addEventListener('click', function() {
-                logForTeam(this.getAttribute('data-team'));
-            });
+    // Reference to the team's score in Firestore
+    const teamScoreRef = db.collection('teams').doc(team);
+
+    db.runTransaction((transaction) => {
+        return transaction.get(teamScoreRef).then((teamScoreDoc) => {
+            if (!teamScoreDoc.exists) {
+                throw "Document does not exist!";
+            }
+
+            // Calculate the new score
+            let newScore = (teamScoreDoc.data().score || 0) + 1;
+
+            // Set the new score
+            transaction.update(teamScoreRef, { score: newScore });
+
+            return newScore;
         });
+    }).then((newScore) => {
+        console.log(`New score for the ${team} team is ${newScore}`);
+    }).catch((error) => {
+        console.log("Transaction failed: ", error);
+    });
 
-        generateQuestion(); // Initial question
-    }
+    document.getElementById('team-options').style.display = 'none';
+    feedbackElement.textContent = '';
+    generateQuestion();
+}
+
+
+      
+
 });
